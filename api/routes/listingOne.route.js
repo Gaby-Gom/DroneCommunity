6f8dcde5390e628f3c13feaf721ba97646179f49
Gaby-Gom <la_gaby_gomez@hotmail.com>
2024-09-13 15:49:46 -0300

    resolviendo error

--- api/routes/listingOne.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { verifyToken } from '../utils/verifyUser.js';
-import { createListingOne } from '../controllers/ListingOne.controller.js';
-const router = express.Router();
-
-router.post('/create', verifyToken, createListingOne);
-
-export default router;

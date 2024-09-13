227091ef48f313440b5026ec71d36f68ab910185
Gaby-Gom <la_gaby_gomez@hotmail.com>
2024-09-13 16:04:58 -0300

    solucion error

--- api/index.js
+++ api/index.js
@@ -4,7 +4,6 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
-import listingOneRouter from './routes/listingOne.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 dotenv.config();
@@ -33,7 +32,6 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
-app.use('/api/listingOne', listingOneRouter);
 
 app.use(express.static(path.join(__dirname, '/drones/dist')));
 
